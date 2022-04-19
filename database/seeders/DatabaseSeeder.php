e5fd77ac1e8215a67941ffca733db7e2451393a5
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-04-19 09:33:35 -0500

    add: Se agregaron seeders para los roles y un usuario admin por defecto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Rol;
+use App\Models\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Rol::create([ 
+            'nombre_rol'=>'bajo nivel'
+        ]);
+        Rol::create([ 
+            'nombre_rol'=>'usuario normal'
+        ]);
+        Rol::create([ 
+            'nombre_rol'=>'administrador'
+        ]);
+
+        User::create([
+            'username_usuario' => 'admin',
+            'nombre_usuario' => 'Diego',
+            'apellidos_usuario' => 'Dueñez',
+            'email_usuario' => 'diego@gmail.com',
+            'password_usuario' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'numero_usuario' => '+528711223529',
+            'email_verified'=>'1',
+            'rol_id' => '3',
+        ]);
     }
 }
